f428a5d3b87912910fb59293ace99dc720b5ba6d
Eliot Shytaj <eliotshytaj05@gmail.com>
2023-10-31 00:02:22 -0400

    Declared the nessecary methods and classes on GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,52 @@
 import javax.swing.JPanel;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+
     GamePanel() {
+
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
