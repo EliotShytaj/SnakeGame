1f85fc738b1fcd0d49d3e69e0b8273477082e88d
Eliot Shytaj <eliotshytaj05@gmail.com>
2023-10-31 00:45:57 -0400

    Finished check collision method.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -49,6 +49,16 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE );
+            }
+            else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE );
+            }
+        }
     }
     public void newApple(){
         appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
@@ -79,7 +89,29 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void checkCollisions() {
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
 
+        if(x[0] < 0) {
+            running = false;
+        }
+
+        if(x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        if(y[0] < 0) {
+            running = false;
+        }
+        if(y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        if(!running) {
+            timer.stop();
+        }
     }
     public void gameOver(Graphics g) {
 
@@ -87,6 +119,12 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter{
